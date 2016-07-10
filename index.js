acce043d2d070eebd522361ebd36e1da2996db12
Daniel Riera <danielriera.p@gmail.com>
2016-07-10 21:48:24 +0200

    Update to res folder

--- index.js
+++ index.js
@@ -44,15 +44,15 @@ var getPlatforms = function (projectName) {
   platforms.push({
     name : 'android',
     isAdded : fs.existsSync('platforms/android'),
-    iconsPath : 'platforms/android/res/',
+    iconsPath : 'www/res/icon/android',
     icons : [
-      { name : 'drawable/icon.png',       size : 96 },
-      { name : 'drawable-hdpi/icon.png',  size : 72 },
-      { name : 'drawable-ldpi/icon.png',  size : 36 },
-      { name : 'drawable-mdpi/icon.png',  size : 48 },
-      { name : 'drawable-xhdpi/icon.png', size : 96 },
-      { name : 'drawable-xxhdpi/icon.png', size : 144 },
-      { name : 'drawable-xxxhdpi/icon.png', size : 192 }
+      { name : 'icon.png',       size : 96 },
+      { name : 'drawable-hdpi-icon.png',  size : 72 },
+      { name : 'drawable-ldpi-icon.png',  size : 36 },
+      { name : 'drawable-mdpi-icon.png',  size : 48 },
+      { name : 'drawable-xhdpi-icon.png', size : 96 },
+      { name : 'drawable-xxhdpi-icon.png', size : 144 },
+      { name : 'drawable-xxxhdpi-icon.png', size : 192 }
     ]
   });
   platforms.push({
